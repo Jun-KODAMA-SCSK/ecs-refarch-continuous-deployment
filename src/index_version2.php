2cb38826e78be7f587b8483acda7517c920550ed
Jun KODAMA <j.kodama@scsk.jp>
2023-09-21 15:57:51 +0900

    Version1

--- src/index_version2.php
+++ src/index_version2.php
@@ -4,7 +4,8 @@
 <title>PHP Sample</title>
 </head>
 <body>
-    Version 01 2023/09/21 15:00
+    Version 02 2023/09/21 16:35
+    <p><img src="github.png"><img src="codepipeline.png"><img src="fargate.png">
 <?php echo gethostname(); ?>
 </body>
 </html>
