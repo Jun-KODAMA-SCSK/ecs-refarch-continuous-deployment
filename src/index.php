34dfb2241521a97522097c1a4af5f74e50bcbcb3
Jun KODAMA <j.kodama@scsk.jp>
2023-09-21 15:36:26 +0900

    version2 add images

--- src/index.php
+++ src/index.php
@@ -4,7 +4,8 @@
 <title>PHP Sample</title>
 </head>
 <body>
-    Version 01 2023/09/21 11:06
+    Version 02 2023/09/21 16:35
+    <p><img src="./github.png"><img src="./codepipeline.png"><img src="./fargategit.png">
 <?php echo gethostname(); ?>
 </body>
 </html>
